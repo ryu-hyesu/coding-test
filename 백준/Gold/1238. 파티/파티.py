b34558d3c9551a850eec5d4e4c8dc9e6595fe8f6
ryu-hyesu <83686474+ryu-hyesu@users.noreply.github.com>
2024-07-04 12:22:24 +0900

    [Gold III] Title: 파티, Time: 224 ms, Memory: 115744 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,7 +1,7 @@
 import math
 import heapq
 from collections import defaultdict
-def dijkstra(start, target) :
+def dijkstra(graph, start) :
     q = []
     heapq.heappush(q, (0, start))
     distances = [math.inf] * (n + 1)
@@ -18,34 +18,25 @@ def dijkstra(start, target) :
             if distance < distances[next_node] : # 갱신
                 distances[next_node] = distance
                 heapq.heappush(q, (distance, next_node))
-
-    # 1 .. n -> target
-    if start != target :
-        return distances[target]
-    # target -> 1 ... n
+                
     return distances
 
 # 입력
 n, m, target = map(int, input().split())
-graph = defaultdict(list)
+_graph = defaultdict(list)
+reverse_graph = defaultdict(list)
 
 for _ in range(m) :
     a, b, cost = map(int, input().split())
-    graph[a].append((b,cost))
-
-result = defaultdict(int)
-
+    _graph[a].append((b,cost))
+    reverse_graph[b].append((a, cost))
+    
 # target -> 1 ... n
-result = dijkstra(target, target)
+result = dijkstra(_graph, target)
 
 # 1 ... n -> target
-for i in range(1, n + 1) :
-    if i != target :
-        result[i] += dijkstra(i, target)
-
-result[0] = 0
-result[target] = 0
+result2 = dijkstra(reverse_graph, target)
 
 # 결과 출력
-print(max(result))
+print(max(result[i] + result2[i] for i in range(1, n + 1) if i != target))
 
