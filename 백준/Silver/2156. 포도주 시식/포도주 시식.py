696eae0047c1e997ca902262d9d245a9f2a7bd98
ryu-hyesu <83686474+ryu-hyesu@users.noreply.github.com>
2024-07-15 21:21:37 +0900

    [Silver I] Title: 포도주 시식, Time: 44 ms, Memory: 34264 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,24 +1,28 @@
 import math
 import sys
 input = sys.stdin.read
+
 data = input().split()
 n = int(data[0])
-wine = list(map(int, data[1:n+1]))
+data = list(map(int, data[1:n+1]))
 
-# DP array to store the maximum wine amount
 dp = [0] * n
 
 if n == 1:
-    print(wine[0])
+    print(data[0])
     sys.exit()
 
-dp[0] = wine[0]
-dp[1] = wine[0] + wine[1]
+dp[0] = data[0]
+dp[1] = data[0] + data[1]
 
 if n > 2:
-    dp[2] = max(wine[2] + wine[0], wine[2] + wine[1], dp[1])
+    dp[2] = max(data[2] + data[0], data[2] + data[1], dp[1])
 
+# 현재 마시지 않는 경우 dp[i - 1]
+# 현재 마시는 경우
+# (1) 이전 잔은 마시지 않는 경우 dp[i - 2] + data[i]
+# (2) 이전 잔도 마시는 경우 dp[i - 3] + data[i - 1] + data[i]
 for i in range(3, n):
-    dp[i] = max(dp[i-1], dp[i-2] + wine[i], dp[i-3] + wine[i-1] + wine[i])
+    dp[i] = max(dp[i-1], dp[i-2] + data[i], dp[i-3] + data[i-1] + data[i])
 
 print(dp[n-1])
