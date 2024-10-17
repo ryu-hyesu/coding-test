11dc8e98fc5a2252d630ea96e83bb965e8526150
ryu-hyesu <83686474+ryu-hyesu@users.noreply.github.com>
2024-10-18 01:25:37 +0900

    [Gold V] Title: 택배 배송, Time: 1452 ms, Memory: 56436 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.py
+++ 백준/Gold/5972. 택배 배송/택배 배송.py
@@ -1,32 +1,35 @@
-from collections import defaultdict
 import math
 import heapq
+from collections import defaultdict
 
-def dijkstra(start) :
+def dijkstra(graph, start, end, n):
+    distances = [math.inf] * (n + 1)
+    distances[start] = 0
     q = []
-    dist = [math.inf] * (n + 1)
-    heapq.heappush(q, (0, start))
+    predecessors = [0] * (n + 1)  # 경로확인
+    heapq.heappush(q, (0, start))  # q에 (cost, node)를 힙정렬로 넣는다.
     
-    while q :
-        curr_cost, curr_node = heapq.heappop(q)
+    while q:
+        current_distance, current_node = heapq.heappop(q)
         
-        if curr_cost > dist[curr_node] :
+        # 만약 연결하려고 하는 현재 노드까지의 거리가(start -> current_node) 지금 distance보다 길다면 continue
+        if current_distance > distances[current_node]:
             continue
-                
-        for next_cost, next_node in graph[curr_node] :
-            cost = next_cost + curr_cost
             
-            if cost < dist[next_node] :
-                dist[next_node] = cost
-                heapq.heappush(q, (cost, next_node))
-                    
-    return dist[n]
+        for next_node, next_cost in graph[current_node]:
+            distance = current_distance + next_cost  # 현재 노드로 연결됐을 때 갱신되는 노드
+            
+            if distance < distances[next_node]:
+                distances[next_node] = distance  # 갱신
+                heapq.heappush(q, (distance, next_node))
+    
+    return distances[end]
 
 n, m = map(int, input().split())
 graph = defaultdict(list)
-for _ in range(m) :
-    a, b, cost = map(int, input().split())
-    graph[a].append((cost, b))
-    graph[b].append((cost, a))
-        
-print(dijkstra(1))
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+    graph[b].append((a, c))
+    
+print(dijkstra(graph, 1, n, n))
