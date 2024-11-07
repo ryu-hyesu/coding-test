ae5743e982b1b1906ad1106a0d97ce5152865f7f
ryu-hyesu <83686474+ryu-hyesu@users.noreply.github.com>
2024-11-07 23:50:13 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 300 ms, Memory: 129452 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,39 +1,22 @@
 import sys
-input = sys.stdin.read
+input = sys.stdin.readline
 
-data = input().split()
-index = 0
+n, m = map(int, input().split())
+data = []
+dp = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
 
-n = int(data[index])
-index += 1
-m = int(data[index])
-index += 1
+for _ in range(n) :
+    _list = list(map(int, input().split()))
+    data.append(_list)
 
-array = []
-for _ in range(n):
-    row = list(map(int, data[index:index+n]))
-    array.append(row)
-    index += n
+for i in range(1, n + 1) :
+    for j in range(1, n + 1) :
+        dp[i][j] = data[i - 1][j - 1] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1]
 
-dp = [[0] * (n + 1) for _ in range(n + 1)]
+for _ in range(m) :
+    x1, y1, x2, y2 = map(int, input().split())
 
-# DP 배열 채우기
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        dp[i][j] = array[i - 1][j - 1] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1]
-
-results = []
-for _ in range(m):
-    x1 = int(data[index])
-    index += 1
-    y1 = int(data[index])
-    index += 1
-    x2 = int(data[index])
-    index += 1
-    y2 = int(data[index])
-    index += 1
-    
     result = dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1]
-    results.append(result)
+    print(result)
 
-sys.stdout.write("\n".join(map(str, results)) + "\n")
+    
