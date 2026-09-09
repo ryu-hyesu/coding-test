e46a0b383a225672bfc70d387becce65270be003
ryu-hyesu <83686474+ryu-hyesu@users.noreply.github.com>
2026-09-09 23:43:14 +0900

    [level 2] Title: 문자열 압축, Time: 1.08 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -0,0 +1,21 @@
+def solution(s):
+    answer = len(s)
+    
+    for x in range(1, len(s) // 2 + 1) :
+        comp_len = 0
+        comp = ''
+        cnt = 1
+        
+        for i in range(0, len(s), x) :
+            temp = s[i:i+x]
+            if comp == temp : cnt += 1
+            elif comp != temp :
+                comp_len += len(comp)
+                if cnt > 1 : comp_len += len(str(cnt))
+                comp = temp
+                cnt = 1
+        
+        comp_len += len(comp) + (len(str(cnt)) if cnt > 1 else 0)
+        answer = min(answer, comp_len)
+            
+    return answer
